61319debf35f1f922b3e88d5f7d93842c1ae0a5b
cogdev <incognitodadev@gmail.com>
2025-09-16 14:27:35 +0300

    New branch for release

--- Rubik Solver Code CMaked/main.cpp
+++ Rubik Solver Code CMaked/main.cpp
@@ -1,253 +1,105 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
-#include <opencv2/opencv.hpp>
-#include <opencv2/highgui.hpp>
-#include <opencv2/videoio.hpp>
 #include <cmath>
 #include <fstream>
 #include <Windows.h>
 #include <cstring>
 
 #include "RubikCube.h" // Rubik Cube Simulator
-#include "Webcam.h" // Webcam for Scanning Colored Squares
 #include "AStarSearch.h" // A* Algorithm for solving the cube
 #include "PuzzleState.h" // This is for hashing the cube in a way the A* understands and solves
-#include "SerialPort.h" // Required for communication between Host and Arduino Uno
 #include "main.h"
 
 using namespace std;
-using namespace cv;
 
 
 enum Color
 {
-    White = 0,
-    Blue = 1,
-    Yellow = 2,
-    Red = 3,
-    Green = 5,
-    Orange = 4
+	White = 0,
+	Blue = 1,
+	Yellow = 2,
+	Red = 3,
+	Green = 5,
+	Orange = 4
 };
 
 void ResetSearch(AStarSearch astarsearch, PuzzleState startnode)
 {
-    astarsearch = AStarSearch();
-    startnode = PuzzleState();
+	astarsearch = AStarSearch();
+	startnode = PuzzleState();
 }
 
 void RubikCubeSimulator(RubikCube RC)
 {
-    SerialPort Uno;
-    WCHAR Port[] = L"\\\\.\\COM5";
+	AStarSearch algo;
+	PuzzleState startnode;
 
-    AStarSearch algo;
-    PuzzleState startnode;
+	bool play = true, syncMovesWithRealRC = false;
+	char input[256];
 
-    bool play = true, syncMovesWithRealRC = false;
-    char input[256];
+	char q[9][256] = { "B F D B F2 U' D2", "L U2 B' L' D' B' L2 B U2 R U' B2 L' R F2 B2 U D2 B R' B2 R' U2 R' L' " };
 
-    char q[9][256] = {"B F D B F2 U' D2", "L U2 B' L' D' B' L2 B U2 R U' B2 L' R F2 B2 U D2 B R' B2 R' U2 R' L' "};
+	while (play)
+	{
+		cout << endl << "Choose a move!" << endl; // ('0' Stop | '-'  Sync with Real Rubik | '+' Solve Algorithmetically the Rubik)"<<endl;
+		cin.getline(input, 256);
+		int i = 0;
 
-    while (play)
-    {
-        cout << endl << "Choose a move!" << endl; // ('0' Stop | '-'  Sync with Real Rubik | '+' Solve Algorithmetically the Rubik)"<<endl;
-        cin.getline(input, 256);
-        int i = 0;
+		switch (input[0]) // Settings beforehand
+		{
+		case '0': // Stop the Simulator
+			cout << endl << "Ending the simulator... (BYE!!!)" << endl;
+			play = false;
+			i++;
+			break;
 
-        switch (input[0]) // Settings beforehand
-        {
-        case '0': // Stop the Simulator
-            cout << endl << "Ending the simulator... (BYE!!!)" << endl;
-            play = false; 
-            i++;
-            break;
+		case '+': // Solve current Rubik Cube
+			cout << endl << "Initializing the solver..." << endl;
+			ResetSearch(algo, startnode);
+			startnode.RC.copyRC(startnode.RC.rc, RC.rc);
 
-        case '-': // Repeat (or stop repeating) moves to Arduino Uno throught serial port
-            syncMovesWithRealRC = !syncMovesWithRealRC;
+			RC.copyRC(RC.rc, algo.SearchSolution(startnode).RC.rc);
 
-            if (syncMovesWithRealRC)
-            {
-                Uno.Initialize(Port, 9600);
-                if (!Uno.isConnected())
-                {
-                    cout << endl << "Failed to connect... Retry the command again...?" << endl;
-                    syncMovesWithRealRC = false;
-                }
-            }
-            else Uno.disconnect();
+			i++;
+			break;
 
-            cout << endl << "Setting -> Repeat RC Moves to Uno: " << syncMovesWithRealRC<<endl;
-            i++;
-            break;
+		default:
+			if (input[0] > '0' && input[0] <= '9')
+			{
+				cout << endl << "Scrambling the code in a predetermined pattern: " << q[input[0] - '0' - 1] << endl;
+				strcpy(input, q[input[0] - '0' - 1]);
+				i++;
+			}
+			break;
+		}
 
-        case '+': // Solve current Rubik Cube
-            cout << endl << "Initializing the solver..." << endl;
-            ResetSearch(algo, startnode);
-            startnode.RC.copyRC(startnode.RC.rc, RC.rc);
-
-            RC.copyRC(RC.rc, algo.SearchSolution(startnode).RC.rc);
-
-            i++;
-            break;
-
-        default:
-            if (input[0] > '0' && input[0] <= '9')
-            {
-                cout << endl << "Scrambling the code in a predetermined pattern: " << q[input[0] - '0' - 1] << endl;
-                strcpy(input, q[input[0] - '0' - 1]);
-                i++;
-            }
-            break;
-        }
-
-        // Now the algorithm for interpreting the actions
-        RC.queueMove(RC, input, i);
-        RC.ReadRubik(RC.dir, RC.rc); 
-
-        if (syncMovesWithRealRC)
-        {
-            Uno.transmit((unsigned char*)input, 8);
-        }
-    }
+		// Now the algorithm for interpreting the actions
+		RC.queueMove(RC, input, i);
+		RC.ReadRubik(RC.dir, RC.rc);
+	}
 }
 
 int main()
 {
-    int SquareCheck = 1, ColorCheck = 1, RubikGame = 1, FileInputOverride = 1;
-    VideoCapture cam;
-    Mat frame; bool ok = 1;
+	RubikCube RC;
 
-    RubikCube RC;
-    Webcam Cam;
+	ifstream fin("RubikCube.txt");
+	int z;
+	for (int l = 0; l < 6; l++)
+		for (int y = 0; y < 3; y++)
+			for (int x = 0; x < 3; x++)
+			{
+				fin >> z;
+				RC.rc[l][y][x] = z;
+			}
 
-    if (!FileInputOverride)
-    {
-        cam = VideoCapture(1);
+	fin.close();
 
-        if (!cam.isOpened())
-        {
-            cout << "No cam detected...";
-            return 0;
-        }
-    }
-
-    int nr = 0;
-    while (ok && !FileInputOverride)
-    {
-        cam >> frame;
-        if (frame.empty()) // Check if the frame is empty
-        {
-            cout << "Error: Captured frame is empty!" << endl;
-            break;
-        }
-        
-        vector<vector<Point>> sq;
-        vector<int> colorType;
+	RC.rotateFaceToMatchOrientation(RC.dir, RC.rc, 0);
+	RC.ReadRubik(RC.dir, RC.rc);
+	RubikCubeSimulator(RC); // Start simulating a Rubik Cube in console
 
 
-        if (RubikGame)
-        {
-            // translate the squares to data as input (while also filtering the extras)
-            Cam.findSquaresAlt(frame, sq, colorType);
-            Cam.filterExtraContours(sq, colorType);
-
-            // debug -> mark individually the squares gathered as data
-            if (SquareCheck)
-                Cam.drawSquares(frame, sq);
-
-            // debug -> render individually the color gathered as data (minus white)
-            if (ColorCheck)
-            {
-                Mat color = Cam.CombineColorFilterResults(frame, vector<int>({ Blue, Yellow, Red, Green, Orange }));
-                addWeighted(frame, 0.5, color, 1.0, 0.0, frame);
-            }
-
-            // translate and position the squares into a 3x3 matrix (and repeat it to the console)
-            int a[3][3];
-            Cam.translateFaceColor(frame, sq, colorType, a);
-            for (int i = 0; i < 3; i++)
-            {
-                for (int j = 0; j < 3; j++)
-                    cout << RC.intTocharColorType(a[i][j]) << " ";
-
-                cout << endl;
-            }
-
-            // user will position the rubik cube and press 'r' when sure to save input
-            if (waitKey(500) == 'r')
-            {
-                for (int i = 0; i < 3; i++)
-                    for (int j = 0; j < 3; j++)
-                        RC.rc[nr][i][j] = a[i][j];
-
-                cout << "Input saved (" << nr+1 << "/6)" << endl;
-                cout << endl;
-
-                nr++;
-            }
-
-            switch (nr)
-            {
-            case 0: // frontFace
-                cout << "f: White / u: Blue";
-                break;
-            case 1: // upFace
-                cout << "f: Blue / u: Yellow";
-                break;
-            case 2: // backFace
-                cout << "f: Yellow / u: Blue";
-                break;
-            case 3: // leftFace
-                cout << "f: Red / u: Blue";
-                break;
-            case 4: // rightFace
-                cout << "f: Orange / u: Blue";
-                break;
-            case 5: // bottomFace
-                cout << "f: Green / u: White";
-                break;
-            case 6:
-                ok = 0;
-                cout << "Creating an image of rubik cube 2D..." << endl;
-                break;
-            }
-
-            cout << endl;
-        }
-        
-        imshow("Webcam", frame);
-        
-        if (waitKey(300) == 'q') ok = 0; // Stop the program 
-    }
-
-    if(!FileInputOverride)
-    {
-        cam.release();
-        destroyAllWindows();
-    }
-    
-    if (FileInputOverride)
-    {
-        ifstream fin("RubikCube.txt");
-        int z;
-        for (int l = 0; l < 6; l++)
-            for (int y = 0; y < 3; y++)
-                for (int x = 0; x < 3; x++)
-                {
-                    fin >> z;
-                    RC.rc[l][y][x] = z;
-                }
-
-        fin.close();
-    }
-
-    if (RubikGame)
-    {
-        RC.rotateFaceToMatchOrientation(RC.dir, RC.rc, 0);
-        RC.ReadRubik(RC.dir, RC.rc);
-        RubikCubeSimulator(RC); // Start simulating a Rubik Cube in console
-    }
-
-    return 0;
+	return 0;
 }
